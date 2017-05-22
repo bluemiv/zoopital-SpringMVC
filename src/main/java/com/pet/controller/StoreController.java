19d0fa0deb2790fc1d2c3367011139c901341dee
Sin seung kyu <seungfff@naver.com>
2017-05-22 15:00:14 +0900

    검색기능 수정!

--- src/main/java/com/pet/controller/StoreController.java
+++ src/main/java/com/pet/controller/StoreController.java
@@ -40,21 +40,21 @@ public class StoreController {
 	@RequestMapping("/selectAll.pet")
 	public ModelAndView getSelectAll(PageDTO input_pageDTO) {
 		ModelAndView mav = new ModelAndView("/store/adminList");
-	
-		// 처음 리스트를 출력할때는 pageNum은 비어있으므로, 초기값 1을 줌 
+
+		// 처음 리스트를 출력할때는 pageNum은 비어있으므로, 초기값 1을 줌
 		int pageNum;
-		if(input_pageDTO.getPageNum() == 0){
+		if (input_pageDTO.getPageNum() == 0) {
 			pageNum = 1;
 		} else {
 			pageNum = input_pageDTO.getPageNum();
 		}
-		
+
 		// 전체 지점 리스트 가져오기
 		StoreDAO dao = sqlSession.getMapper(StoreDAO.class);
-		
+
 		// 리스트 개수를 가져옴
 		int totalNum = dao.getListCount();
-		
+
 		// startNum과 endNum을 구함
 		final int pageSize = 5;
 		int startNum = 1 + pageSize * (pageNum - 1);
@@ -62,13 +62,13 @@ public class StoreController {
 		PageDTO pageDTO = new PageDTO();
 		pageDTO.setStartNum(startNum);
 		pageDTO.setEndNum(endNum);
-		
+
 		// 페이지에 맞는 리스트를 가져옴
 		List<StoreDTO> adminList = dao.getPageSelectAll(pageDTO);
-		
+
 		// 전체 리스트의 총 페이지 개수
 		int totalPageNum;
-		if(totalNum % pageSize == 0){
+		if (totalNum % pageSize == 0) {
 			// 5의 배수
 			totalPageNum = totalNum / pageSize;
 		} else {
@@ -117,17 +117,17 @@ public class StoreController {
 
 		return mav;
 	}
-	//store_name 으로 검색기능
+
+	// store_name 으로 검색기능
 	@RequestMapping("/search.pet")
-	public ModelAndView searchStore(StoreDTO dto){
+	public ModelAndView searchStore(StoreDTO dto) {
 		ModelAndView mav = new ModelAndView("/store/adminList");
-		StoreDAO dao = sqlSession.getMapper(StoreDAO.class);	
+		StoreDAO dao = sqlSession.getMapper(StoreDAO.class);
 		System.out.println("searchList 실행되었습니다.");
 		List searchList = dao.searchStore(dto);
-		mav.addObject("searchList",searchList);
-		
+		mav.addObject("searchList", searchList);
+
 		return mav;
 	}
-	
 
 }
